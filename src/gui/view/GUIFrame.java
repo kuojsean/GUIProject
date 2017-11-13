ce3b7562bcd0ae6b143cea6c108248e069745947
kuojsean <kuo.j.sean@gmail.com>
2017-11-13 08:05:16 -0700

    Created the Frame Class.

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -1,6 +1,25 @@
 package gui.view;
 
-public class GUIFrame
-{
+import gui.controller.GUIAppController;
+import javax.swing.JFrame;
 
+public class GUIFrame extends JFrame
+{
+	private GUIAppController appController;
+	
+	public GUIFrame(GUIAppController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		
+		this.setTitle("The Window Title");
+		this.setSize(500, 500);
+		this.setVisible(true);
+	}
 }
