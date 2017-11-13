6cfc6cf56dbcf681d3b034b9e43465555f1f12e6
kuojsean <kuo.j.sean@gmail.com>
2017-11-13 08:38:52 -0700

    Finished JFrame package and put helper methods into the JPanel so it shows a button.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -15,6 +15,35 @@ public class GUIPanel extends JPanel
 		super();
 		this.appController = appController;
 		firstButton = new JButton("Wow a button");
+		
+		setupPanel();
 	}
 	
+	/**
+	 * Used to add all the components to the subclass of JPanel
+	 * This installs them into the panel so they are seen in the GUI.
+	 */
+	
+	private void setupPanel()
+	{
+		this.add(firstButton);
+		
+	}
+	
+	/**
+	 * This is used to hold all the layout and arrangement code for SpringLayout
+	 */
+	private void setupLayout()
+	{
+		
+	}
+	
+	
+	/**
+	 * This helper method is used to link any GUI components to the associated listener
+	 */
+	private void setupListeners()
+	{
+		
+	}
 }
